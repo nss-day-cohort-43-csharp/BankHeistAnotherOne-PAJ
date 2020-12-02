26a72e47f268e98c4801bbbb608a73785f145415
Jerry Robertson <jerry.r.robertson1@gmail.com>
2020-12-02 14:55:06 -0600

    Can choose as many allies as you want, as long as they don't recieve too much of the cut!

--- Program.cs
+++ Program.cs
@@ -1,138 +1,144 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BankHeist
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Hacker Joe = new Hacker();
-      Joe.Name = "Joe";
-      Joe.SkillLevel = 76;
-      Joe.PercentageCut = 40;
-      Hacker Thomas = new Hacker();
-      Thomas.Name = "Thomas";
-      Thomas.SkillLevel = 69;
-      Thomas.PercentageCut = 20;
-      Muscle Frank = new Muscle();
-      Frank.Name = "Frank";
-      Frank.SkillLevel = 49;
-      Frank.PercentageCut = 15;
-      Muscle Greg = new Muscle();
-      Greg.Name = "Greg";
-      Greg.SkillLevel = 75;
-      Greg.PercentageCut = 40;
-      LockSpecialist Jonny = new LockSpecialist();
-      Jonny.Name = "Jonny";
-      Jonny.SkillLevel = 49;
-      Jonny.PercentageCut = 15;
-      LockSpecialist Sarah = new LockSpecialist();
-      Sarah.Name = "Sarah ManHanderson";
-      Sarah.SkillLevel = 70;
-      Sarah.PercentageCut = 35;
+        static void Main(string[] args)
+        {
+            Hacker Joe = new Hacker();
+            Joe.Name = "Joe";
+            Joe.SkillLevel = 76;
+            Joe.PercentageCut = 40;
+            Hacker Thomas = new Hacker();
+            Thomas.Name = "Thomas";
+            Thomas.SkillLevel = 69;
+            Thomas.PercentageCut = 20;
+            Muscle Frank = new Muscle();
+            Frank.Name = "Frank";
+            Frank.SkillLevel = 49;
+            Frank.PercentageCut = 15;
+            Muscle Greg = new Muscle();
+            Greg.Name = "Greg";
+            Greg.SkillLevel = 75;
+            Greg.PercentageCut = 40;
+            LockSpecialist Jonny = new LockSpecialist();
+            Jonny.Name = "Jonny";
+            Jonny.SkillLevel = 49;
+            Jonny.PercentageCut = 15;
+            LockSpecialist Sarah = new LockSpecialist();
+            Sarah.Name = "Sarah ManHanderson";
+            Sarah.SkillLevel = 70;
+            Sarah.PercentageCut = 35;
 
 
 
-      List<IRobber> rolodex = new List<IRobber>
+            List<IRobber> rolodex = new List<IRobber>
             {
                 Joe, Thomas, Frank, Greg, Jonny, Sarah
             };
 
-      List<IRobber> crew = new List<IRobber> { };
+            List<IRobber> crew = new List<IRobber> { };
 
-      void printRolodox()
-      {
-        foreach (IRobber recruit in rolodex)
-        {
-          Console.WriteLine($"Enter {rolodex.IndexOf(recruit)} for -- {recruit.Name} -- Specialty: {recruit.getSpec()} -- Skill Level: {recruit.SkillLevel} -- Percentage Cut: {recruit.PercentageCut}");
-        }
-      }
-      void printCrew()
-      {
-        Console.WriteLine("Here's your crew.");
-        foreach (IRobber recruit in crew)
-        {
-          Console.WriteLine($"{recruit.Name} -- Specialty: {recruit.getSpec()} -- Skill Level: {recruit.SkillLevel} -- Percentage Cut: {recruit.PercentageCut}");
-        }
-      }
-
-      // This is the naming and creation of new crew members in the rolodex - hirable members
-      while (true)
-      {
-        Console.WriteLine($"Current Operatives: {rolodex.Count}");
-        Console.Write("What's the new operative's name: ");
-        string name = Console.ReadLine();
-        if (name.Equals(""))
-        {
-          break;
-        }
-        Console.WriteLine("");
-        Console.Write($"{name}'s specialty is(Hacker,Muscle,LockSpecialist): ");
-        string speciality = Console.ReadLine();
-        Console.WriteLine("");
-        while (true)
-        {
-          Console.Write($"This {speciality}'s skill level between 1 and 100: ");
-          string tempSkill = Console.ReadLine();
-          int skLvl;
-          if (Int32.TryParse(tempSkill, out skLvl))
-          {
-            if (speciality.Equals("Hacker"))
+            void printRolodox(List<IRobber> crew)
             {
-              rolodex.Add(new Hacker()
-              {
-                Name = name,
-                SkillLevel = skLvl
-              });
+                foreach (IRobber recruit in rolodex)
+                {
+                    if (!crew.Exists(x => x.Name == recruit.Name) && crew.Sum(x => x.PercentageCut) + recruit.PercentageCut <= 100)
+                    {
+                        Console.WriteLine($"Enter {rolodex.IndexOf(recruit)} for -- {recruit.Name} -- Specialty: {recruit.getSpec()} -- Skill Level: {recruit.SkillLevel} -- Percentage Cut: {recruit.PercentageCut}");
+                    }
+                }
+
             }
-            else if (speciality.Equals("Muscle"))
+            void printCrew()
             {
-              rolodex.Add(new Muscle()
-              {
-                Name = name,
-                SkillLevel = skLvl
-              });
+                Console.WriteLine("Here's your crew.");
+                foreach (IRobber recruit in crew)
+                {
+                    Console.WriteLine($"{recruit.Name} -- Specialty: {recruit.getSpec()} -- Skill Level: {recruit.SkillLevel} -- Percentage Cut: {recruit.PercentageCut}");
+                }
             }
-            else if (speciality.Equals("LockSpecialist"))
+
+            // This is the naming and creation of new crew members in the rolodex - hirable members
+            while (true)
             {
-              rolodex.Add(new LockSpecialist()
-              {
-                Name = name,
-                SkillLevel = skLvl
-              });
+                Console.WriteLine($"Current Operatives: {rolodex.Count}");
+                Console.Write("What's the new operative's name: ");
+                string name = Console.ReadLine();
+                if (name.Equals(""))
+                {
+                    break;
+                }
+                Console.WriteLine("");
+                Console.Write($"{name}'s specialty is(Hacker,Muscle,LockSpecialist): ");
+                string speciality = Console.ReadLine();
+                Console.WriteLine("");
+                while (true)
+                {
+                    Console.Write($"This {speciality}'s skill level between 1 and 100: ");
+                    string tempSkill = Console.ReadLine();
+                    int skLvl;
+                    if (Int32.TryParse(tempSkill, out skLvl))
+                    {
+                        if (speciality.Equals("Hacker"))
+                        {
+                            rolodex.Add(new Hacker()
+                            {
+                                Name = name,
+                                SkillLevel = skLvl
+                            });
+                        }
+                        else if (speciality.Equals("Muscle"))
+                        {
+                            rolodex.Add(new Muscle()
+                            {
+                                Name = name,
+                                SkillLevel = skLvl
+                            });
+                        }
+                        else if (speciality.Equals("LockSpecialist"))
+                        {
+                            rolodex.Add(new LockSpecialist()
+                            {
+                                Name = name,
+                                SkillLevel = skLvl
+                            });
+                        }
+                        break;
+                    }
+
+
+                }
+
             }
-            break;
-          }
+            Random random = new Random();
+            Bank bank = new Bank(random.Next(50000, 1000001), random.Next(101), random.Next(101), random.Next(101));
+            List<string> recon = bank.compair();
+            System.Console.WriteLine($"The most secure system is {recon[0]}");
+            System.Console.WriteLine($"The least secure system is {recon[1]}");
 
-
+            //   This will be the while loop for creating a crew.
+            while (true)
+            {
+                printRolodox(crew);
+                Console.WriteLine("");
+                Console.WriteLine("Who would you like to recruit to your crew?");
+                try
+                {
+                    string input = Console.ReadLine();
+                    if (input == "") { break; }
+                    int userChoice = Int32.Parse(input);
+                    crew.Add(rolodex[userChoice]);
+                }
+                catch
+                {
+                    Console.WriteLine("Did you enter a whole number?");
+                }
+            }
+            printCrew();
         }
-
-      }
-      Random random = new Random();
-      Bank bank = new Bank(random.Next(50000, 1000001), random.Next(101), random.Next(101), random.Next(101));
-      List<string> recon = bank.compair();
-      System.Console.WriteLine($"The most secure system is {recon[0]}");
-      System.Console.WriteLine($"The least secure system is {recon[1]}");
-      printRolodox();
-
-      //   This will be the while loop for creating a crew.
-      while (true)
-      {
-        Console.WriteLine("");
-        Console.WriteLine("Who would you like to recruit to your crew?");
-        try
-        {
-          int userChoice = Int32.Parse(Console.ReadLine());
-          crew.Add(rolodex[userChoice]);
-          break;
-        }
-        catch
-        {
-          Console.WriteLine("Did you enter a whole number?");
-        }
-      }
-      printCrew();
     }
-  }
 }
